a7ad04ff84ae3b18090fa35cc7dd46caf19dd3a8
jemise111 <jesse.sessler@gmail.com>
2014-04-08 16:27:39 -0400

    first commit

---
+++ redt_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RedtApp::Application.config.secret_key_base = '6d14f555e4431aab5e50cc2bfe017648c8d53aba1f0591e14573e969e6142a7e08dbc3c4d7038da78b6adaca4ba338e6420ac78516749934e7c0afcbe8df4b4e'
